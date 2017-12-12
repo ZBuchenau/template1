2c18bac887caa1aab8731c9c604d38e84e4f3020
Zach Buchenau <ZBuchenau@yahoo.com>
2017-12-12 16:20:02 -0700

    added the main banner carousel headline templates and styling

--- js/main.js
+++ js/main.js
@@ -66,23 +66,23 @@ $(document).ready(function() {
   var bottom_of_object = $(elem).offset().top + $(elem).outerHeight();
   var object_height = $(elem).height();
   var bottom_of_window = $(window).height();
-  var position = $(window).scrollTop()+bottom_of_window;
+  var position = $(window).scrollTop() + (bottom_of_window + (object_height/2));
   console.log(position);
-  if( position > bottom_of_object){
-      $(elem).animate({'opacity': 1}, 2000);
+  if( position > (bottom_of_object - object_height)){
+      $(elem).animate({'opacity': 1}, 1000);
+      // $(elem).show( "slide", {direction: "up" }, 2000 );
       console.log('scrollTop: ', position);
       console.log('bottom_of_object: ', bottom_of_object);
 
       console.log("here we are");
   } else {
       $(window).scroll( function(){
-        var scroll = $(window).scrollTop() + (bottom_of_window + (object_height / 2));
+        var scroll = $(window).scrollTop() + (bottom_of_window + (object_height));
           if(scroll >= (bottom_of_object)){
-              $(elem).animate({'opacity': 1}, 1500);
+              $(elem).animate({'opacity': 1}, 1000);
+              // $(elem).show( "slide", {direction: "up" }, 2000 );
               console.log("here we are");
-              $(elem).finish();
           }
-          $(elem).finish();
       });
   }
 });
