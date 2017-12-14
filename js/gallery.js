443a6fe48542db9aa47cf9e365842fc03874b00a
Zach Buchenau <ZBuchenau@yahoo.com>
2017-12-14 16:10:40 -0700

    deleted unnecessary code

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,2 @@
-//
-
-  // var wall = new Freewall("#galleryWall");
-
-
-	// wall.reset({
-	// 	selector: '.brick',
-	// 	animate: true,
-	// 	// cellW: 1000,
-	// 	// cellH: 100,
-  //   keepOrder: true,
-	// 	onResize: function() {
-	// 		wall.fitWidth();
-	// 	}
-	// });
-  // wall.container.find('.brick img').on(function() {
-  //   wall.fitZone();
-  // });
-
-
 
 //
